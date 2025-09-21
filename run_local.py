202214376f35972e154f035991003018473c76f4
edrubikovich001-ops <edrubikovich001@gmail.com>
2025-09-21 18:55:38 +0500

    Update run_local.py

--- run_local.py
+++ run_local.py
@@ -1,4 +1,7 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
+load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
